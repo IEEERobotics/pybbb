b08b26e95d6dc79ed9660e124f7e6918d3031ee2
Daniel Farrell <dfarrell07@gmail.com>
2013-08-16 03:40:19 -0400

    Added custom __str__ method, direction getter

--- bbb/gpio.py
+++ bbb/gpio.py
@@ -4,8 +4,12 @@
 class GPIO(object):
 
     def __init__(self, num, base_dir='/sys/class/gpio/gpio'):
-        self.sysfs = base_dir + str(num)
-        self._value = None
+        self.num = num
+        self.sysfs = base_dir + str(self.num)
+
+    def __str__(self):
+        return "GPIO #{}: value:{}, direction:{}".format(self.num, self.value,
+                                                         self.direction)
 
     @property
     def value(self):
@@ -16,6 +20,15 @@ class GPIO(object):
     def value(self, value):
         with open(self.sysfs + '/value', 'w') as f:
             f.write(str(value) + '\n')
+
+    @property
+    def direction(self):
+        with open(self.sysfs + '/direction', 'r') as f:
+            direction = f.read()
+            # Remove trailing newline for readability
+            if direction[-1] == "\n":
+                direction = direction[:-1]
+            return direction
 
     def input(self):
         with open(self.sysfs + '/direction', 'w') as f:
